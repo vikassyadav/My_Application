577968996661722239a6e4852d24fa450a641873
vikassyadav <vikasyadav2702@gmail.com>
2023-10-23 23:19:51 +0530

    THIRD commit

--- app/src/main/java/com/example/myapplication/parentChildOpt.java
+++ app/src/main/java/com/example/myapplication/parentChildOpt.java
@@ -26,7 +26,7 @@ CardView cardView1 ,cardView2 ;
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        Intent iHome = new Intent(parentChildOpt.this , MainActivity.class);
+                        Intent iHome = new Intent(parentChildOpt.this , parentDashboard.class);
                         startActivity(iHome);
                         //finish();//poping from stack so is activity can't get open even after pressing back button
 
@@ -41,7 +41,7 @@ CardView cardView1 ,cardView2 ;
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        Intent iHome = new Intent(parentChildOpt.this , MainActivity.class);
+                        Intent iHome = new Intent(parentChildOpt.this , childDashboard.class);
                         startActivity(iHome);
                         //finish();//poping from stack so is activity can't get open even after pressing back button
 
